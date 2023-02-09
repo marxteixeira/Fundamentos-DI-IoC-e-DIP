a71946ce91704475c5c21dcb1d6fb00bfe994a0e
Marx Teixeira <95075808+marxteixeira@users.noreply.github.com>
2023-02-08 22:27:24 -0300

    finalizando repository e services

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -40,45 +40,13 @@ public class OrderController : ControllerBase
         // #2 - Calcula o frete
         var deliveryFee = await _deliveryFeeService.GetDeliveryFeeAsync(zipCode);
         var cupon = await _promoCodeRepository.GetPromoCodeAsync(promoCode);
+        var discount = cupon?.Value ?? 0M;
 
         // #3 - Calcula o total dos produtos
-        decimal subTotal = 0;
-        const string getProductQuery = "SELECT [Id], [Name], [Price] FROM PRODUCT WHERE ID=@id";
-        for (var p = 0; p < products.Length; p++)
-        {
-            Product product;
-            await using (var conn = new SqlConnection("CONN_STRING"))
-                product = await conn.QueryFirstAsync<Product>(getProductQuery, new { Id = p });
+        var order = new Order(deliveryFee, discount, new List<Product>());
+        return Ok($"Pedido {order.Code} gerado com sucesso!");
 
-            subTotal += product.Price;
-        }
-
-        // #4 - Aplica o cupom de desconto
-        decimal discount = 0;
-        await using (var conn = new SqlConnection("CONN_STRING"))
-        {
-            const string query = "SELECT * FROM PROMO_CODES WHERE CODE=@code";
-            var promo = await conn.QueryFirstAsync<PromoCode>(query, new { code = promoCode });
-            if (promo.ExpireDate > DateTime.Now)
-                discount = promo.Value;
-        }
-
-        // #5 - Gera o pedido
-        var order = new Order();
-        order.Code = Guid.NewGuid().ToString().ToUpper().Substring(0, 8);
-        order.Date = DateTime.Now;
-        order.DeliveryFee = deliveryFee;
-        order.Discount = discount;
-        order.Products = products;
-        order.SubTotal = subTotal;
-
-        // #6 - Calcula o total
-        order.Total = subTotal - discount + deliveryFee;
-
-        // #7 - Retorna
-        return Ok(new
-        {
-            Message = $"Pedido {order.Code} gerado com sucesso!"
-        });
+        
+       
     }
 }
